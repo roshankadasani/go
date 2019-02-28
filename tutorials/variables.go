33f3246a9fc76b0a4ddcbbba6127b91176f80406
Roshan Kadasani <roshan_123@csu.fullerton.edu>
2019-02-27 22:43:44 -0500

    rearranged

--- tutorials/variables.go
+++ tutorials/variables.go
@@ -7,4 +7,13 @@ func main()  {
   num = 20.2
   fmt.Println(num)
   fmt.Printf("num is of type %T\n", num)
+
+  var a = "initial"
+  fmt.Println(a)
+
+  var b,c int = 1,2
+  fmt.Println(b, c)
+
+  var d = true
+  fmt.Println(d)
 }
